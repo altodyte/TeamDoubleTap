fc376bffa81197b9cb2e1e48fbee766cf8e70ad7
Bill Warner <billwarner13@gmail.com>
2013-04-21 18:25:02 -0400

    calibration function (more or less done)
    also fixed index bugs (thanks to Emily)

--- WithoutDelay/WithoutDelay.ino
+++ WithoutDelay/WithoutDelay.ino
@@ -1,12 +1,12 @@
 // Setup Variables
 const int trigger = HIGH; // unused
 // Sets Node Pin #'s
-const int nodes[] = {1,2,3,4};
+const int nodes[] = {2,4,7,8};
 // record processing variables
 boolean reading[4] = {0,0,0,0}; // storage for values recorded by digital pins
 int time[4] = {0,0,0,0}; // storage for times of triggers
 int flag[4] = {0,0,0,0}; // storage for trigger state indicator
-int first = 0; // storage for seed node number (first triggered in cycle)
+int first = -1; // storage for seed node number (first triggered in cycle)
 // triangulation variables
 int pixel = 100; // number of pixels across width
 
@@ -20,17 +20,26 @@ void setup() {
 }
 
 void loop() {
+  // Read values of digital pins
+  // Check if any new nodes have exceeded trigger voltage
+  read_and_check_all();
+  // Enters signal processing if all nodes have reached trigger (sensed a tap)
+  if ((flag[0]==1)&&(flag[1]==1)) {
+    process();
+  }
+}
+
+void read_and_check_all() {
   // Reads values of digital pins
   for (int i=0; i<4; i++) {
     reading[i] = digitalRead(nodes[i]);
   }
+  // TAKE OUT WHEN USING ALL NODES
+  reading[2] = LOW;
+  reading[3] = LOW;
   // Checks if any new nodes have exceeded trigger voltage
   for (int i=0; i<4; i++) {
     read_node(i);
-  }
-  // Enters signal processing if all nodes have reached trigger (sensed a tap)
-  if ((flag[0]==1)&&(flag[1]==1)) {
-    process();
   }
 }
 
@@ -39,7 +48,7 @@ void read_node(int num) {
   if ((reading[num] == HIGH)&&(flag[num]==0)) {
     time[num] = micros(); // records time of trigger
     flag[num] = 1; // indicates that this node has triggered
-    if (first==0) {
+    if (first==-1) {
       first = num; // checks if this is the first node to trigger (seed)
     }
   }
@@ -47,27 +56,48 @@ void read_node(int num) {
 
 void process() {
   // More detailed signal processing required for more nodes
+  if (first==0) {
+    Serial.println("Node 0");
+  }
   if (first==1) {
     Serial.println("Node 1");
   }
-  if (first==2) {
-    Serial.println("Node 2");
-  }
   // Reset Flag Values (to untriggered)
   for (int i=0; i<4; i++) {
-    flag[i] = 0;
+    flag[i] = 0; // resets all the flags
   }
+  first = -1; // resets the first seed
 }
 
 void calibrate() {
-  int calibrate[4][3];
-  Serial.println("Please tap node 1.");
-  while ((flag[0]==0)||(flag[1]==0)) {
-    for (int i=0; i<4; i++) {
-      read_node(i);
+  int calibrate[4][3]; // Creates a 4x3 array to store calibration data (speeds)
+  int cal_index[4][3] = {{1,2,3},{0,2,3},{0,1,3},{0,1,2}}; // keeps track of first index of calibrate to write to
+  int cal_counter[4]; // keeps track of second index of calibrate to write to
+  int cal_distance[4][3] = {{1,sqrt(2),1},{1,1,sqrt(2)},{sqrt(2),1,1},{1,sqrt(2),1}}; 
+  int caltimes[4][4]; // Creates a 4x4 array to store calibration data (times)
+  int equalized_times[4][4]; // Storage for equalized times (with respect to seed time)
+  for (int i=0; i<4; i++) {
+    Serial.println("Please tap node "); // figure out how to stringify ints "tap node i"
+    while ((flag[0]==0)||(flag[1]==0)) { // ||(flag[2]==0)||(flag[3]==0)
+      read_and_check_all();
+    }
+    caltimes[i] = time;
+    // need to reset flags, first
+  }
+  for (int i=0; i<4; i++) {
+    for (int j=0; j<4; j++) {
+      equalized_times[i][j] = caltimes[i][j] - caltimes[i][i];
     }
   }
-  for (int i=0; i<3; i++) {
-    calibrate[0][i] = time[i+1] - time[0]; // seed time - node time
+  for (int x=0; x<4; x++) {
+    for (int i=0; i<3; i++) {
+      int dist = cal_distance[cal_index[x][i]][cal_counter[cal_index[x][i]]]*pixel;
+      int t = equalized_times[x][cal_index[x][i]];
+      int val = dist/t; // might have to be a float
+      calibrate[cal_index[x][i]][cal_counter[cal_index[x][i]]];
+      cal_counter[cal_index[x][i]]++;
+    }
   }
+  // need to reset flags, init. vals.
+  first = -1;
 }
